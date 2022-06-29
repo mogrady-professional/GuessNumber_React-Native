c538c579c18a25f9bdf444a4e096fca2a2e64a99
Michael O'Grady <mogrady.professional@gmail.com>
2022-06-29 21:20:34 +0100

    Dimensions API Added for sizing impovements

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -10,12 +10,15 @@ export default Title;
 
 const styles = StyleSheet.create({
   title: {
+    fontFamily: "open-sans-bold",
     fontSize: 24,
-    fontWeight: "bold",
+    // fontWeight: "bold",
     color: Colors.accent500,
     textAlign: "center",
     borderWidth: 2,
     borderColor: Colors.accent500,
     padding: 12,
+    maxWidth: "80%",
+    minWidth: 300,
   },
 });
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -113,6 +113,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24,
+    alignItems: "center",
   },
   instructionText: {
     marginBottom: 12,
